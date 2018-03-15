c388aa6075a3c356e69c4bc2d9286ebe04e40b2e
Jonas Devlieghere <jonas@devlieghere.com>
2018-03-15 16:21:16 +0000

    [dsymutil] Rename llvm-dsymutil -> dsymutil
    Now that almost all functionality of Apple's dsymutil has been
    upstreamed, the open source variant can be used as a drop in
    replacement. Hence we feel it's no longer necessary to have the llvm
    prefix.

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -139,7 +139,7 @@ tools = [
 # FIXME: Why do we have both `lli` and `%lli` that do slightly different things?
 tools.extend([
     'lli', 'lli-child-target', 'llvm-ar', 'llvm-as', 'llvm-bcanalyzer', 'llvm-config', 'llvm-cov',
-    'llvm-cxxdump', 'llvm-cvtres', 'llvm-diff', 'llvm-dis', 'llvm-dsymutil',
+    'llvm-cxxdump', 'llvm-cvtres', 'llvm-diff', 'llvm-dis', 'dsymutil',
     'llvm-dwarfdump', 'llvm-extract', 'llvm-isel-fuzzer', 'llvm-opt-fuzzer', 'llvm-lib',
     'llvm-link', 'llvm-lto', 'llvm-lto2', 'llvm-mc', 'llvm-mca','llvm-mcmarkup',
     'llvm-modextract', 'llvm-nm', 'llvm-objcopy', 'llvm-objdump',
